365a5b6adb8a2d29696be398ea11f0f3f5ff8f62
Aleksandr Poliakov <master@artwist.ru>
2024-03-01 16:45:30 +0300

    создаёт проект на swiftui

---
+++ Rzhd/Rzhd/RzhdApp.swift
@@ -0,0 +1,17 @@
+//
+//  RzhdApp.swift
+//  Rzhd
+//
+//  Created by Александр Поляков on 01.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct RzhdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
