0afac6bca624c8a139619f4f49aa700fd8d3c5c4
Aleksandr Poliakov <master@artwist.ru>
2024-03-06 22:17:47 +0300

    создаёт класс для поиска маршрутов

--- Rzhd/Rzhd/ContentView.swift
+++ Rzhd/Rzhd/ContentView.swift
@@ -29,20 +29,29 @@ struct ContentView: View {
                  transport: URLSessionTransport()
              )
 
-             let service = NearestStationsService(
+             let service = RoutesSearchService(
                  client: client,
                  apikey: "3b033964-4652-469c-bcda-c5e26afbc1b4"
              )
 
-             Task {
-                 do {
-                     let stations = try await service.getNearestStations(lat:
-     59.864177, lng: 30.319163, distance: 50)
-                     print(stations)
-                 } catch {
-                     print("Error fetching stations: \(error)")
-                 }
-             }
+//             Task {
+//                 do {
+//                     let stations = try await service.getNearestStations(lat:
+//     59.864177, lng: 30.319163, distance: 50)
+//                     print(stations)
+//                 } catch {
+//                     print("Error fetching stations: \(error)")
+//                 }
+//             }
+        
+            Task {
+                do {
+                    let result = try await service.search(from:"c213", to: "c239", date: "2024-03-08")
+                    print(result)
+                } catch {
+                    print("Error fetching stations: \(error)")
+                }
+            }
          }
     
 }
---
+++ Rzhd/Rzhd/Routes.swift
@@ -0,0 +1,37 @@
+//
+//  Routes.swift
+//  Rzhd
+//
+//  Created by Александр Поляков on 06.03.2024.
+//
+
+// 1. Импортируем библиотеки
+import OpenAPIRuntime
+import OpenAPIURLSession
+
+typealias Routes = Components.Schemas.Routes
+
+protocol RoutesSearchProtocol {
+  func search(from: String, to: String, date: String) async throws -> Routes
+}
+
+final class RoutesSearchService: RoutesSearchProtocol {
+  private let client: Client
+  private let apikey: String
+  
+  init(client: Client, apikey: String) {
+    self.client = client
+    self.apikey = apikey
+  }
+  
+func search(from: String, to: String, date: String) async throws -> Routes {
+
+    let response = try await client.search(query: .init(
+        apikey: apikey,
+        from: from,
+        to: to,
+        date: date
+    ))
+    return try response.ok.body.json
+  }
+}
