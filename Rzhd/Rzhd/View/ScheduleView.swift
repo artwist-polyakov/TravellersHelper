2800fe1e8454468715e61f0b5d27805d6b6836c2
Aleksandr Poliakov <master@artwist.ru>
2024-04-12 14:20:36 +0300

    выносит вьюхи для таб бара в отдельные классы

---
+++ Rzhd/Rzhd/View/ScheduleView.swift
@@ -0,0 +1,20 @@
+//
+//  ScheduleView.swift
+//  Rzhd
+//
+//  Created by Александр Поляков on 12.04.2024.
+//
+
+import SwiftUI
+
+struct ScheduleView: View {
+    var body: some View {
+        // Тут будет логика экрана Расписания
+        Text("Расписание")
+            .edgesIgnoringSafeArea(.top)
+    }
+}
+
+#Preview {
+    ScheduleView()
+}
