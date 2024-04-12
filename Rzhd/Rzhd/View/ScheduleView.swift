6eea95b631c294d97db354992308eec35eb2c136
Aleksandr Poliakov <master@artwist.ru>
2024-04-12 18:05:25 +0300

    полностью конфигурирует поля ввода

--- Rzhd/Rzhd/View/ScheduleView.swift
+++ Rzhd/Rzhd/View/ScheduleView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ScheduleView: View {
+    @State private var fromText: String = ""
+    @State private var toText: String = ""
     var body: some View {
         VStack {
             Spacer()
@@ -17,12 +19,31 @@ struct ScheduleView: View {
                     .frame(height: 128)
                     .cornerRadius(20)
                 HStack {
-                    Rectangle()
-                        .fill(Color.white)
-                        .cornerRadius(20)
-                        .frame(height: 96)
-                        .padding(16) // Отступ слева
+                    VStack {
+                        TextField("Откуда", text: $fromText)
+                            .font(.system(size: 17))
+                            .foregroundColor(.gray)
+                            .textFieldStyle(.plain)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding([.horizontal, .top])
+                            .multilineTextAlignment(.leading)
+                        Spacer()
+                        
+                        TextField("Куда", text: $toText)
+                            .font(.system(size: 17))
+                            .foregroundColor(.gray)
+                            .textFieldStyle(.plain)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding([.horizontal, .top])
+                            .multilineTextAlignment(.leading)
 
+                        Spacer()
+                    }
+                    .frame(height: 96)
+                    .background(Color.white.cornerRadius(20))
+                    .padding(16) // Отступ слева
+                    
+                    
                     
                     
                     Image("Refresh")
