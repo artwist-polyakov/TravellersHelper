85a0eb6d4ff466188cad02f72c55324f3d390afa
Aleksandr Poliakov <master@artwist.ru>
2024-04-17 14:40:46 +0300

    создаёт вьюмодель для вывода городов

---
+++ Rzhd/Rzhd/Models/City.swift
@@ -0,0 +1,13 @@
+//
+//  City.swift
+//  Rzhd
+//
+//  Created by Александр Поляков on 17.04.2024.
+//
+
+import Foundation
+
+struct City: Hashable, Identifiable {
+    var id = UUID()
+    var name: String
+}
