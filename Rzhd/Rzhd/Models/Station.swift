c1ad8d77a3b4741129679c65816e7deba9f49bf6
Aleksandr Poliakov <master@artwist.ru>
2024-04-21 20:27:56 +0300

    добавляет модель и вьюмодель для станций

---
+++ Rzhd/Rzhd/Models/Station.swift
@@ -0,0 +1,13 @@
+//
+//  Station.swift
+//  Rzhd
+//
+//  Created by Александр Поляков on 21.04.2024.
+//
+
+import Foundation
+
+struct Station: Hashable, Identifiable {
+    var id = UUID()
+    var name: String
+}
