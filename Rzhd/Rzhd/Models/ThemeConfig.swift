6e16610b5388eddaf1c7f83be1a0a0c9feb14105
Aleksandr Poliakov <master@artwist.ru>
2024-04-22 23:53:58 +0300

    добавляет элементы для отображения настроек

---
+++ Rzhd/Rzhd/Models/ThemeConfig.swift
@@ -0,0 +1,13 @@
+//
+//  ThemeConfig.swift
+//  Rzhd
+//
+//  Created by Александр Поляков on 22.04.2024.
+//
+
+import Foundation
+
+class ThemeConfig: ObservableObject {
+    @Published var isDarkMode: Bool = false
+}
+
