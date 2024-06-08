0ddd63bfcbdf764d753b73c20d5160a0b69b5d6f
Aleksandr Poliakov <master@artwist.ru>
2024-06-09 00:58:22 +0300

    добавляет коллбек в конфиг таймера

--- Rzhd/Rzhd/Algorithms/TimerConfiguration.swift
+++ Rzhd/Rzhd/Algorithms/TimerConfiguration.swift
@@ -11,14 +11,17 @@ struct TimerConfiguration {
     let storiesCount: Int
     let timerTickInternal: TimeInterval
     let progressPerTick: CGFloat
+    let onFinishCallback: () -> Void
 
     init(
         storiesCount: Int,
         secondsPerStory: TimeInterval = 5,
-        timerTickInternal: TimeInterval = 0.25
+        timerTickInternal: TimeInterval = 0.25,
+        onFinishCallback: @escaping () -> Void
     ) {
         self.storiesCount = storiesCount
         self.timerTickInternal = timerTickInternal
         self.progressPerTick = 1.0 / CGFloat(storiesCount) / secondsPerStory * timerTickInternal
+        self.onFinishCallback = onFinishCallback
     }
 }
