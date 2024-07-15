06b7770fbe5782735a45b6de332f8afe61eb4838
Aleksandr Poliakov <master@artwist.ru>
2024-07-15 21:34:45 +0300

    создаёт класс для репозицтория поиска

---
+++ Rzhd/Rzhd/Data/Search/SearchRepository.swift
@@ -0,0 +1,13 @@
+//
+//  SearchRepository.swift
+//  Rzhd
+//
+//  Created by Александр Поляков on 15.07.2024.
+//
+
+import Foundation
+import OpenAPIURLSession
+
+actor SearchRepository {
+    static let shared = SearchRepository()
+}
